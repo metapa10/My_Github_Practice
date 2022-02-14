51fe4a826b901350998a70ae98547146c5747148
parth6807 <98125079+parth6807@users.noreply.github.com>
2022-02-14 00:42:08 -0500

    Time: 812 ms (61.70%), Space: 15.2 MB (88.86%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -6,22 +6,23 @@ class Solution:
         def children(lock):
             res = []
             for i in range(4):
-                digit = str((int(lock[i]) + 1) % 10)
-                res.append(lock[:i] + digit + lock[i+1:])
-                digit = str((int(lock[i]) - 1 + 10) % 10)
-                res.append(lock[:i] + digit + lock[i+1:])
+                digit = str((int(lock[i]) + 1) % 10) 
+                res.append(lock[:i] + digit + lock[i + 1:])
+                digit = str((int(lock[i]) - 1 + 10) % 10) 
+                res.append(lock[:i] + digit + lock[i + 1:])
             return res
-        
-        
         q = deque()
-        q.append(["0000", 0]) #lock,number of turns
-        visit = set(deadends)  #hash set
+        q.append(["0000", 0]) #lock, turns
+        visit = set(deadends)
+        
         while q:
-            lock, number_of_turns = q.popleft()
+            lock, turns = q.popleft()
             if lock == target:
-                return number_of_turns
+                return turns
             for child in children(lock):
                 if child not in visit:
                     visit.add(child)
-                    q.append([child, number_of_turns + 1])
+                    q.append([child, turns + 1])
+                    
         return -1
+        
